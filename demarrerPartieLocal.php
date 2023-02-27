07d623c643cfedecfd41f31cb4dd98fed9678384
ThekingOfTotem <72319321+ThekingOfTotem@users.noreply.github.com>
2023-02-27 14:48:48 +0100

    ;

--- demarrerPartieLocal.php
+++ demarrerPartieLocal.php
@@ -1,7 +1,28 @@
+
+
+<form method="get" action="resultats.php">
+	<label for="joueur1">Nom du joueur 1:</label>
+	<input type="text" id="joueur1" name="joueur1"><br>
+		
+	<label for="joueur2">Nom du joueur 2:</label>
+	<input type="text" id="joueur2" name="joueur2"><br>
+		
+	<input type="submit" value="Valider">
+</form>
+
 <?php
 
 session_start();
 $_SESSION["Joueur1"] = $_GET["name1"];
 $_SESSION["Joueur2"] = $_GET["name2"];
 
-$grille;
+$grille = array(
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 1
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 2
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 3
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 4
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 5
+    array(0, 0, 0, 0, 0, 0, 0) // ligne 6
+);
+
+$_SESSION["grille"]=$grille;
