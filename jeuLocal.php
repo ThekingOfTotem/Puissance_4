8d91f30a5811835155106a64e41522e26de1563e
ThekingOfTotem <72319321+ThekingOfTotem@users.noreply.github.com>
2023-02-27 14:52:29 +0100

    Create jeuLocal.php

---
+++ jeuLocal.php
@@ -0,0 +1,32 @@
+<?php
+
+$grille = array(
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 1
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 2
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 3
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 4
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 5
+    array(0, 0, 0, 0, 0, 0, 0) // ligne 6
+);
+
+function coupAutorise($grille){
+
+
+}
+
+//echo json_encode($grid);
+function afficherGrille($grille) {
+    for ($i = 0; $i < count($grille); $i++) {
+        for ($j = 0; $j < count($grille[$i]); $j++) {
+            echo "| " . $grille[$i][$j] . " ";
+        }
+        echo "|<br>";
+    }
+    //echo "- --------------<br>";
+}
+
+
+$_SESSION["Joueur1"] = $nameplayer1;
+$_SESSION["Joueur2"] = $nameplayer2;
+$_SESSION["grille"] = $grille;
+afficherGrille($grille);
