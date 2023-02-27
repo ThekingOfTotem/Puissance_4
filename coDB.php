418cfea726608cede506f4729c4f953683e0acb1
ThekingOfTotem <72319321+ThekingOfTotem@users.noreply.github.com>
2023-02-27 13:38:04 +0100

    ,

--- coDB.php
+++ coDB.php
@@ -5,8 +5,14 @@ function seConnecter(){
     //code erreur si un soucis
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-
     return $pdo;
 }
-
 ?>
+
+prouet
+
+blap
+blep
+blip
+blop
+blup
---
+++ jeuLocal.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+$grid = array(
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 1
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 2
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 3
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 4
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 5
+    array(0, 0, 0, 0, 0, 0, 0) // ligne 6
+);
+
+echo $grid;
