d04b5f7f652c1717d51d796cfd07d866a963f93b
TheKingOfTotem <floriandebort@gmail.com>
2023-03-05 21:55:50 +0100

    zer

--- LOCAL/demarrerPartieLocal.php
+++ LOCAL/demarrerPartieLocal.php
@@ -9,7 +9,7 @@
 
 <body>
 
-<form method="get" action="jeuLocal.php">
+<form method="get" action="jouerCoup.php">
     <!--On demande le nom des joueurs-->
 	<label for="Joueur1">Nom du joueur 1 : </label>
 	<input type="text" id="joueur1" name="joueur1"><br>
@@ -20,6 +20,7 @@
 	<input type="submit" value="Valider">
 </form>
 <?php
+//on initialise notre grille vide
 $grille = array(
     array(0, 0, 0, 0, 0, 0, 0), // ligne 1
     array(0, 0, 0, 0, 0, 0, 0), // ligne 2
@@ -28,6 +29,8 @@ $grille = array(
     array(0, 0, 0, 0, 0, 0, 0), // ligne 5
     array(0, 0, 0, 0, 0, 0, 0) // ligne 6
 );
+
+//on attribue la grille et joueurs a la nouvelle partie 
 $_SESSION["grille"] = $grille;
 $_SESSION["Joueur1"] = $joueur1;
 $_SESSION["Joueur2"] = $joueur2;
--- jouerCoup.php
+++ jouerCoup.php
@@ -1,4 +1,15 @@
 <?php
 session_start();
-$grille = $_SESSION["grille"];
+$grille = $_POST["grille"];
 $_SESSION["nomJoueur"] = $_GET["nom"];
+$_SESSION["col"] = $_GET["colonne"];
+
+//Verifier l'autorisation du coup 
+echo count($grille);
+function coupPossible($grille, $col){
+    for ($i = 0; $i < count($grille); $i++) {
+        
+    }
+
+
+}
