2e15563a12ff7385ca213bc26cd6f0d1deba3820
TheKingOfTotem <floriandebort@gmail.com>
2023-03-05 21:32:15 +0100

    Update demarrerPartieLocal.php

--- demarrerPartieLocal.php
+++ demarrerPartieLocal.php
@@ -10,6 +10,7 @@
 <body>
 
 <form method="get" action="jeuLocal.php">
+    <!--On demande le nom des joueurs-->
 	<label for="Joueur1">Nom du joueur 1 : </label>
 	<input type="text" id="joueur1" name="joueur1"><br>
 		
@@ -18,8 +19,19 @@
 		
 	<input type="submit" value="Valider">
 </form>
-
-
+<?php
+$grille = array(
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 1
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 2
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 3
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 4
+    array(0, 0, 0, 0, 0, 0, 0), // ligne 5
+    array(0, 0, 0, 0, 0, 0, 0) // ligne 6
+);
+$_SESSION["grille"] = $grille;
+$_SESSION["Joueur1"] = $joueur1;
+$_SESSION["Joueur2"] = $joueur2;
+?>
 </body>
 
 </html>
