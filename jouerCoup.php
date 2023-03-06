f424a6492cdde6c81833324900fff4a8b772a289
ThekingOfTotem <72319321+ThekingOfTotem@users.noreply.github.com>
2023-03-06 08:56:03 +0100

    Update jouerCoup.php

--- jouerCoup.php
+++ jouerCoup.php
@@ -1,15 +1,32 @@
 <?php
 session_start();
 $grille = $_POST["grille"];
-$_SESSION["nomJoueur1"] = $_GET["nom1"];
+$_SESSION["nomJoueur1"] = $_GET["joueur1"];
+$_SESSION["nomJoueur2"] = $_GET["joueur2"];
 $_SESSION["tour"]="Joueur1";
 $_SESSION["col"] = $_GET["colonne"];
 $col = $_SESSION["col"];
-
+$position;
 //Verifier l'autorisation du coup 
 //On vérifie si la colonne n'est pas vide
 function coupPossible($grille, $col){
     return $grille[5][$col] == 0;
+}
+
+//On vérifie si la grille est remplie 
+function grilleRemplie($grille){
+    $cmp = 0;
+    for ($i = 0; $i<7; $i++){
+        if($grille[5][$i]!=0){
+            //On compte le nombre de colonne remplie
+            $cmp++;
+        }
+    }
+    if($cmp==7){
+        return true; // la grille est remplie
+    }else{
+        return false; //la grille n'est pas remplie
+    }
 }
 
 //On joue le coup 
@@ -28,17 +45,15 @@ function jouerCoup($grille,$col){
                     $_SESSION["tour"]=="Joueur1";
                     exit;
                 }
-            }
-            
+            }  
         }
     }else{
         echo "Choisissez une autre colonne";
-    }
-    
+    }   
 }
 
 function victoire($grille){
-
+for ($i = 0)
     
 $_SESSION["grille"] = $grille;
 }
