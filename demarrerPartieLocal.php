9d95e1a3c9183ce704ce096b075c1493b5035f41
juliaoto <79646127+juliaoto@users.noreply.github.com>
2023-03-11 16:51:43 +0100

    Update demarrerPartieLocal.php

--- demarrerPartieLocal.php
+++ demarrerPartieLocal.php
@@ -14,8 +14,8 @@
     <!--On demande le nom des joueurs-->
     <div id="names">
 	    <label for="joueur1">Nom du joueur 1 : </label>
-	    <input type="text" id="joueur1" name="joueur1"><br>
-		<br></br>
+	    <input type="text" id="joueur1" name="joueur1"><pre>
+
 	    <label for="joueur2">Nom du joueur 2 : </label>
 	    <input type="text" id="joueur2" name="joueur2"><br>
     </div>
