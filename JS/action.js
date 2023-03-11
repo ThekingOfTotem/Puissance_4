baf469b96946637e9176a974dc0d72b722f01148
Aporoxis <saitbouqdir@gmail.com>
2023-03-11 12:14:03 +0100

    Update action.js

--- JS/action.js
+++ JS/action.js
@@ -1,6 +1,6 @@
 function traiterClic(nc) {
     //appeler le serv
-    $.get("https://fafa.kroko.xyz/~aitbouqdir/ProjetWeb/validerCoup.php", { joueur: "1", col: nc }, traiterRepServ);
+    $.get("/validerCoup.php", { joueur: "1", col: nc }, traiterRepServ);
 
 }
 
