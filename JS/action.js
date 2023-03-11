31896c54f215eb1017ff24239c4398687dd94b96
ThekingOfTotem <72319321+ThekingOfTotem@users.noreply.github.com>
2023-03-11 11:36:44 +0100

    c

--- JS/action.js
+++ JS/action.js
@@ -1,11 +1,11 @@
-function traiterClic() {
+function traiterClic(nc) {
     //appeler le serv
-    $.get("https://fafa.kroko.xyz/~aitbouqdir/ProjetWeb/validerCoup.php", traiterReponseRev);
+    $.get("https://fafa.kroko.xyz/~aitbouqdir/ProjetWeb/validerCoup.php", { joueur: "1", col: nc }, traiterRepServ);
 
 }
 
 function traiterRepServ(data) {
     //data.grille
     //data.resultat
-
+    //->afficher
 }
