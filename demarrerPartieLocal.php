6aa54c1d3be12ef1d1940ae5d747c0cb5cc3d75b
juliaoto <79646127+juliaoto@users.noreply.github.com>
2023-03-11 16:53:14 +0100

    Update demarrerPartieLocal.php

--- demarrerPartieLocal.php
+++ demarrerPartieLocal.php
@@ -13,11 +13,11 @@
 <form method="get" action="partieLocal.php">
     <!--On demande le nom des joueurs-->
     <div id="names">
-	    <label for="joueur1">Nom du joueur 1 : </label><pre>
-	    <input type="text" id="joueur1" name="joueur1">
+	    <label for="joueur1">Nom du joueur 1 : </label>
+	    <input type="text" id="joueur1" name="joueur1"><pre>
 
-	    <label for="joueur2">Nom du joueur 2 : </label><pre>
-	    <input type="text" id="joueur2" name="joueur2">
+	    <label for="joueur2">Nom du joueur 2 : </label>
+	    <input type="text" id="joueur2" name="joueur2"><pre>
     </div>
 		
 	<input type="submit" value="Valider">
