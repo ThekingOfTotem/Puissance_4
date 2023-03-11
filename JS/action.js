5f1e07e077d4e7e0fa5db3c395753e094a427b3a
ThekingOfTotem <72319321+ThekingOfTotem@users.noreply.github.com>
2023-03-11 12:13:56 +0100

    Update action.js

--- JS/action.js
+++ JS/action.js
@@ -1,11 +1,12 @@
 function traiterClic(nc) {
     //appeler le serv
-    $.get("https://fafa.kroko.xyz/~aitbouqdir/ProjetWeb/validerCoup.php", { joueur: "1", col: nc }, traiterRepServ);
+    $.get("validerCoup.php", { joueur: "1", col: nc }, traiterRepServ);
 
 }
 
 function traiterRepServ(data) {
-    //data.grille
-    //data.resultat
-    //->afficher
+    console.log("colonne" + nc)
+        //data.grille
+        //data.resultat
+        //->afficher
 }
