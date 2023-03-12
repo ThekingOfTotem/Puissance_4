5232610ad4727cfb4bcb5b4bc28b22c92cf0a967
Aporoxis <saitbouqdir@gmail.com>
2023-03-12 15:03:57 +0100

    Update partieLocal.php

--- partieLocal.php
+++ partieLocal.php
@@ -5,6 +5,7 @@
     <title>Puissance 4 Local</title>
     <!--lien avec le css-->
     <link href="CSS/style.css" rel="stylesheet"/>
+    <link href="CSS/style.css" rel="stylesheet"/>
     <script src="JS/jquery-3.6.3.min.js"></script>
     <script src="JS/action.js"></script>
 
@@ -44,7 +45,7 @@
       }
       echo "</div>";
       echo "</div>";
-      
+
     ?>
     
     </div>
