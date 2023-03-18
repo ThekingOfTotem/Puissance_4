df04bbdb3b43371bd1bfb79461c78a9cc03ebe60
Aporoxis <saitbouqdir@gmail.com>
2023-03-18 16:08:10 +0100

    pouet

--- JS/action.js
+++ JS/action.js
@@ -39,16 +39,19 @@ function traiterCoup() {
                     nomJoueurOld=document.getElementById("nomJoueur").textContent;
                     if(data=="autre colonne"){
                       alert("choisissez une autre colonne");
-                    }
-                    if(data==true){
-                      alert("La partie est gagnée par "+nomJoueurOld);
-                      window.location.replace("demarrerPartieLocal.php");
                     }else{
-                      document.getElementById("nomJoueur").textContent=data["nomJoueur"];
                       grille=data["grille"];
                       position=data["position"];
                       var div = document.querySelector('[data-ligne="'+position+'"][data-colonne="'+nc+'"]');
                       div.classList.add(data["couleur"]);
+                      if(data['victoire']==true){
+                        setTimeout(function(){
+                          alert("La partie est gagnée par "+nomJoueurOld);
+                          window.location.replace("demarrerPartieLocal.php");
+                        }, 20);
+                      }else{
+                        document.getElementById("nomJoueur").textContent=data["nomJoueur"];
+                      }
                     }
                 }
               });
