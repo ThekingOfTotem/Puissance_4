b81e19db52c3575f19c67566cee0a6720501a30e
Aporoxis <saitbouqdir@gmail.com>
2023-03-19 15:11:15 +0100

    jouerCoup et action commentés

--- JS/action.js
+++ JS/action.js
@@ -1,15 +1,14 @@
 var nc, j;
 
 function traiterClic(button) {
-  nc = button.getAttribute("data-colonne");
-  j = button.getAttribute("data-joueur");
-  $.ajax({
+  nc = button.getAttribute("data-colonne"); // On récupère la colonne du bouton
+  $.ajax({ // On vérifie que le coup soit possible
     url: "validerCoup.php",
     type: "GET",
     data: {
       joueur: j, col: nc
     },
-    success: function(response) {
+    success: function(response) { // Si le coup est possible on l'execute
       traiterCoup()
     }
   });
@@ -17,32 +16,32 @@ function traiterClic(button) {
 
 function traiterCoup() {
     
-  $.ajax({
-    url: "jouerCoup.php",
-    type: "POST",
+  $.ajax({ // On joue le coup
+    url: "jouerCoup.php", // On va executer le script
+    type: "POST", // En envoyant des valeurs en méthode POST
     data: {
-      action:"jouerCoup", col: nc
+      action:"jouerCoup", col: nc // On détaille les valeurs envoyées
     },
     success: function(response) {
-      data = JSON.parse(response);
-      nomJoueurOld=document.getElementById("nomJoueur").textContent;
-      if(data=="autre colonne"){
+      data = JSON.parse(response); // On adapte le contenu JSON
+      nomJoueurOld=document.getElementById("nomJoueur").textContent; // On stock le nom du Joueur aillant joué
+      if(data=="autre colonne"){ // Si la colonne choisie n'est pas adapté
         alert("choisissez une autre colonne");
-      }else{
+      }else{ // Sinon on poursuit les opérations
         grille=data["grille"];
         position=data["position"];
-        var div = document.querySelector('[data-ligne="'+position+'"][data-colonne="'+nc+'"]');
-        div.classList.add(data["couleur"]);
-        if(data['victoire']==true){
+        var div = document.querySelector('[data-ligne="'+position+'"][data-colonne="'+nc+'"]'); // On récupère la case à modifier
+        div.classList.add(data["couleur"]); // On modifie la couleur de la case
+        if(data['victoire']==true){ // Si le coup fait gagner le joueur 
           setTimeout(function(){
-            alert("La partie est gagnée par "+nomJoueurOld);
-            window.location.replace("demarrerPartieLocal.php");
+            alert("La partie est gagnée par "+nomJoueurOld); // On affiche un message
+            window.location.replace("demarrerPartieLocal.php"); // On redirige vers la page de démarrage
           }, 20);
-        }else{
-          document.getElementById("nomJoueur").textContent=data["nomJoueur"];
+        }else{ // Si le coup ne fait pas gagner
+          document.getElementById("nomJoueur").textContent=data["nomJoueur"]; // On change l'affichage pour indiquer le prochain tour
         }
       }
-      $.ajax({
+      $.ajax({ // On verifie que la grille n'est pas remplie
         url: "jouerCoup.php",
         type: "POST",
         data: {
@@ -50,7 +49,7 @@ function traiterCoup() {
         },
         success: function(response) {
           data = JSON.parse(response);
-          if (data == "true") {
+          if (data == "true") { // Si elle est remplie on arrête la partie et on redirige vers la page de démarrage
             alert("La partie se termine sur une égalité !");
             window.location.replace("demarrerPartieLocal.php");
           }
--- jouerCoup.php
+++ jouerCoup.php
@@ -3,10 +3,10 @@ session_start();
 $grille = $_SESSION["grille"];
 $tour = $_SESSION["tour"];
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($_SERVER['REQUEST_METHOD'] === 'GET') { // Si le script reçoit des variable GET
     $col = $_GET["col"];
     
-}else{
+}else{ //Sinon on execute la fonction appelée
     $action = $_POST["action"];
     call_user_func($action);
 }
@@ -45,42 +45,42 @@ function jouerCoup()
   global $position;
   global $grille;
   $col=$_POST["col"];
-  $victoire=false;
+  $victoire=false; // On considère que personne n'a encore gagné
     //Si le coup est possible
     if (coupPossible($grille, $col)) {
         for ($i = 5; $i >= 0; $i--) {
-            if ($grille[$i][$col] == 0) {
+            if ($grille[$i][$col] == 0) {//On cherche en partant du bas de la colonne la prochaine case à remplir
                 $position = $i;
                 if ($_SESSION["tour"] == "Joueur1") {
-                    $grille[$i][$col] = 1;
-                    $_SESSION["grille"] = $grille;
+                    $grille[$i][$col] = 1; //Si c'est au tour du Joueur 1, on rempli la case avec la valeur 1
+                    $_SESSION["grille"] = $grille; // On met à jour la grille
                     if(victoire($grille, 1)){
-                        $victoire=true;
+                        $victoire=true; // Si le coup joué permet de gagner, on change la valeur de la variable victoire
                     }
-                        $_SESSION["tour"] = "Joueur2";
+                        $_SESSION["tour"] = "Joueur2"; // On change de joueur
                         $data = array('couleur'=>'red' ,'nomJoueur' => $_SESSION["nomJoueur2"], 'grille' => $grille, 'position' => $position, 'victoire' => $victoire);
-                        echo json_encode($data);
+                        echo json_encode($data); // On renvoie les valeurs nécessaires 0à la poursuite du jeu
                         exit;
                 } if ($_SESSION["tour"] == "Joueur2"){
-                    $grille[$i][$col] = 2;
-                    $_SESSION["grille"] = $grille;
+                    $grille[$i][$col] = 2; //Si c'est au tour du Joueur 2, on rempli la case avec la valeur 2
+                    $_SESSION["grille"] = $grille; // On met à jour la grille
                     if(victoire($grille, 2)){
-                        $victoire=true;
+                        $victoire=true; // Si le coup joué permet de gagner, on change la valeur de la variable victoire
                     }
-                        $_SESSION["tour"] = "Joueur1";
+                        $_SESSION["tour"] = "Joueur1"; // On change de joueur
                         $data = array('couleur'=>'yellow' ,'nomJoueur' => $_SESSION["nomJoueur1"], 'grille' => $grille, 'position' => $position, 'victoire' => $victoire);
-                        echo json_encode($data);
+                        echo json_encode($data); // On renvoie les valeurs nécessaires 0à la poursuite du jeu
                         exit;
                 }
             }
         }
         
     } else {
-        echo json_encode("autre colonne");
+        echo json_encode("autre colonne"); // Si le coup n'est pas possible
     }
 }
 
-
+// Cette fonction vérifie si le jeton posé permet de gagner la partie
 function victoire($grid, $player) {
     $rows = count($grid);
     $cols = count($grid[0]);
