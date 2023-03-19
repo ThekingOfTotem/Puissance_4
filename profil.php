72e202746e0b7a76c00963d1269df7ea4dae87c6
juliaoto <79646127+juliaoto@users.noreply.github.com>
2023-03-19 17:29:35 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -10,7 +10,8 @@
     $pseudo = $_SESSION['pseudo'];
 
     // récupérer les informations de l'utilisateur à partir de la base de données
-    $req = "SELECT Nom_joueur, Victoire FROM joueur WHERE Nom_joueur = ?";
+    $req = "SELECT * FROM joueur WHERE Nom_joueur = ?";
+    //$req = "SELECT Nom_joueur, Victoire FROM joueur WHERE Nom_joueur = ?";
     $res = $cnx->prepare($req);
     $res->execute([$pseudo]);
     $tabStat = $res->fetch();
@@ -28,22 +29,56 @@
     <link href="CSS/indexStyle.css" rel="stylesheet" />
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <style>
+        /* Style pour centrer le tableau et ajouter un espace avec l'image */
+        table {
+            margin: 20px auto;
+        }
+        .displayed {
+            margin-bottom: 20px;
+        }
+        /* Style pour l'entête du tableau */
+        th {
+            background-color: #FFC107; /* jaune */
+            color: #FFF;
+            padding: 10px;
+        }
+
+        /* Style pour les lignes du tableau */
+        td {
+            border: 1px solid #000; /* noir */
+            padding: 10px;
+        }
+
+        /* Style pour la première ligne du tableau */
+        tr:first-child {
+            background-color: #1976D2; /* bleu */
+            color: #FFF;
+        }
+    </style>
 </head>
 
 
 <body>
     <IMG class="displayed" src="logo.png">
     <div>
-        <h1>Profile de <?php echo $tabStat['Nom_joueur']; ?></h1>
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>Nombre de victoires</th>
+                    <th>Nom</th>
+                    <th>Victoire</th>
+                    <th>Défaite</th>
+                    <th>Nul</th>
+                    <th>Total parties jouées</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
+                    <td><?php echo $tabStat['Nom_joueur']; ?></td>
                     <td><?php echo $tabStat['Victoire']; ?></td>
+                    <td><?php echo $tabStat['Defaite']; ?></td>
+                    <td><?php echo $tabStat['Nul']; ?></td>
+                    <td><?php echo $tabStat['Total_parties_jouees']; ?></td>
                 </tr>
             </tbody>
         </table>
